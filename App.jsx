6afc7367b3195348eecf630107b03386a3d0b5db
ganjamsahithi <168392299+ganjamsahithi@users.noreply.github.com>
2024-11-10 18:13:33 +0530

    integrating frontend added

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Frontend from './components/Frontend'; // Import the Frontend component
+
+const App = () => {
+  return (
+    <div>
+      <Frontend /> {/* Include the Frontend component to handle routes */}
+    </div>
+  );
+};
+
+export default App;
